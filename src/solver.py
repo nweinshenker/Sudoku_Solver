1f90354972dcfaa793588d0e93749fb62040553c
nweinshenker <nweinshenker@wisc.edu>
2021-01-13 18:00:14 -0600

    Built out the testing functionaility for determining if the board is valid

--- src/solver.py
+++ src/solver.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 class Solver:
     def __init__(self) -> None:
         self.rows = 3 
@@ -11,11 +13,40 @@ class Solver:
         - Within a 3x3 grid
             Only one number of [1-9] exits
         '''
-        rows = [0] * (self.rows ** 2)
-        cols = [0] * (self.rows ** 2)
-        print(rows)
-        print(cols)
+        n = len(self.board) # The number of rows and cols
+        
+        # HashSet for the rows, cols & grid of the sudoku puzzle
+        rows = [set() for i in range(9)]
+        cols = [set() for i in range(9)]
+        mMat = [set() for i in range(9)]
+        
+        n = len(self.board)
+
+        for i in range(n):
+            for j in range(n):
+                cur = self.board[i][j]
+                if cur != '':
+                    
+                    k = (i // 3 ) * 3 + j // 3
+                
+                    # Check the given row is valid
+                    if (cur not in rows[i]):
+                         rows[i].add(cur)
+                    else: 
+                        return False
+                    
+                    # Check the given col is valid
+                    if (cur not in cols[j]): 
+                        cols[j].add(cur)
+                    else: 
+                        return False
+                
+                    # Check the given grid is valid
+                    if (cur not in mMat[k]):
+                        mMat[k].add(cur)
+                    else:
+                        return False
+        return True
 
 if __name__ == '__main__':
     solver = Solver()
-    solver.is_valid()
